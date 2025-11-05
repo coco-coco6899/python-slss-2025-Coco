df71e416ee56d066a37a61ab573ca86ec9ae6f87
Coco Woo <cocowoo.slss@gmail.com>
2025-11-05 13:05:25 -0800

    add the notes

---
+++ Notes-6-numbers.py
@@ -0,0 +1,17 @@
+# Numbers and Opetations
+# Author: Coco
+# Nov 5, 2025
+
+# Create an algorithm to gather data to find the most popular bubbe tea place around us
+
+# Version 1
+# Show all the bbt choices
+# Ask the user for their choices
+# Add their vote to a running tally
+# GIVE SOME RAW SCORES
+# Give score as a percentage
+
+# Version 2
+# Ask the user what their fave bbt place is
+# tally
+# give the raw score and give score as percentage
